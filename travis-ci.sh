8e3ac503bc34768ec093bb1adadbb61056e13cfb
George <httpcatharsis@gmail.com>
2017-02-15 16:14:10 +0300

    coveralls

---
+++ travis-ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$COVERAGE" = true ]; then
+    dub test --compiler=${DC} --debug=featureTest -b unittest-cov
+    dub fetch doveralls
+    dub run doveralls --compiler=${DC}
+else
+    dub test --compiler=${DC} --debug=featureTest
+fi
