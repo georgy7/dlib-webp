d622e37c61fd73ca27d2b921a23eb479b6923d0c
George <httpcatharsis@gmail.com>
2017-02-17 13:15:08 +0300

    Travis fail on failure.

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,8 +2,10 @@
 
 if [ "$COVERAGE" = true ]; then
     dub test --compiler=${DC} --debug=featureTest -b unittest-cov
+    e=$?; if [[ $e != 0 ]]; then exit $e; fi
     dub fetch doveralls
     dub run doveralls --compiler=${DC}
 else
     dub test --compiler=${DC} --debug=featureTest
+    e=$?; if [[ $e != 0 ]]; then exit $e; fi
 fi
